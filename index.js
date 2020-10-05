44476ffdd39ed32f49fea3144d9e943b047a2fb2
sakib115 <67532653+sakib115@users.noreply.github.com>
2020-10-05 20:12:42 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const MongoClient = require('mongodb').MongoClient;
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser')
+var cors = require('cors')
+const ObjectId = require('mongodb').ObjectId;
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const uri = "mongodb+srv://sakibsheikh:sakibsakib.@cluster0.6d4vl.mongodb.net/volunteer?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const collection = client.db("volunteer").collection("registrations");
+    app.get('/request/:pname', (req, res) => {
+    collection.find({ name: req.params.pname})
+      .toArray((err, data) => {
+        res.send(data)
+      })
+    })
+    app.post('/addRegistration', (req, res) => {
+    const info = req.body;
+    collection.insertOne(info)
+    .then(result => {
+      res.send('data added successfully')
+    })
+    })
+    app.delete('/delete/:id', (req, res) => {
+    collection.deleteOne({ _id: ObjectId(req.params.id) })
+      .then(result => {
+        res.send('data deleted successfully')
+    })
+    })
+});
+
+app.listen(4500, () => {
+  console.log('port 4500 activated')
+})
